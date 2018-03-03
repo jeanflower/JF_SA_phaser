496d71c8ca37791d0f581a63d49a199c0cce40e1
Jean Flower <jeanflower@rocketmail.com>
2018-03-03 18:36:00 +0000

    phaser game with test_game.py script

---
+++ test_game.py
@@ -0,0 +1,64 @@
+# GATHER REQUIRED FILES
+# create a folder for test environment
+# copy index.html, renamed to a unique filename.html
+# copy assets/paddle.png
+# copy phaser.min.js
+#
+# START WEB SERVER
+# cd test folder
+# python -m http.server
+#
+# REQUEST PAGE
+# open filename.html
+
+# CLEANUP ---------- TODO
+# ps
+# kill -p 'the number of the python -m httpserver process
+
+import os # for sending commands to terminal
+import sys # for getting python command line arguments
+import time 
+
+unique_name = "test_game_"+str(time.time())
+
+# GATHER REQUIRED FILES
+os.system('mkdir '+unique_name)
+os.system('mkdir '+unique_name+"/assets")
+os.system('cp index.html '+unique_name+"/"+unique_name+".html")
+os.system('cp assets/paddle.png '+unique_name+"/assets/paddle.png")
+
+print(sys.argv)
+
+if len(sys.argv) == 1: # no arguments
+    os.system('cp phaser.min.js '+unique_name+"/phaser.min.js")
+elif sys.argv[1] == "new":
+    os.system('cp new_phaser/phaser.min.js '+unique_name+"/phaser.min.js") # doesn't work
+elif sys.argv[1] == "old":
+    os.system('cp old_phaser/phaser.min.js '+unique_name+"/phaser.min.js") 
+
+print("files copied to "+unique_name)
+
+# START WEB SERVER
+import os
+os.system('python -m http.server &')
+
+print("server running")
+
+# REQUEST PAGE
+os.system("open -n -a \"Google Chrome\" --args \"--new-window\" "
+          +"\"http://localhost:8000/"+unique_name+"/"+unique_name+".html\" &")
+
+print("opened web page")
+
+# CLEAN UP
+input("Press Enter to continue...")
+
+# find the process for http.server and kill it
+processes = os.popen("ps").read()
+for process in processes.splitlines():
+    if "http.server" in process:
+        pid = process[1:5]
+        print("killing process id = "+pid)
+        os.system('kill -9 '+pid)
+        
+print("cleaned up")
